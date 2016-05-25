3b901266c74c28a3c927df61ff9de035bf29f795
xiaobing <xiaobing3@staff.sina.com.cn>
2016-05-25 20:50:10 +0800

    解决了demo无发运行的问题

--- XXBImagePickerDemo/2015_02_03_XXBImagePicker/ViewController.m
+++ XXBImagePickerDemo/2015_02_03_XXBImagePicker/ViewController.m
@@ -40,7 +40,7 @@
 - (void)openPhoto
 {
     // 创建一个照片选择器
-    XXBImagePickerController  *imagePickController = [[XXBImagePickerController alloc] initWithChooseMediaType:XXBMediaTypeVideos];
+    XXBImagePickerController  *imagePickController = [[XXBImagePickerController alloc] initWithChooseMediaType:XXBMediaTypePhotos];
     // 设置做多可选的照片数
     imagePickController.photoCount = 100;
     // 是都展示左上角的数字标签
